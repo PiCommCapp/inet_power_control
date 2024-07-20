e93f57d324329587bf419b2586d7cb1effda3cc3
Cappy <163203679+PiCommCapp@users.noreply.github.com>
2024-07-20 18:37:27 +0100

    times

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # config.py
 LOG_DIR = "/home/server/log"
-LOG_FILE = f"{LOG_DIR}/inet_power_control.log"
+LOG_FILE = "{LOG_DIR}/inet_power_control.log"
 PING_COUNT = 15
 FAIL_THRESHOLD = PING_COUNT // 2
 TIME_BETWEEN_STEPS = {
     'initial_pause': 30,
-    'modem_restart': 600,
+    'modem_restart': 60,
     'router_restart': 300,
-    'switch_restart': 3600
+    'switch_restart': 150
 }
 MQTT_ENABLED = False
 MQTT_BROKER = "mqtt_broker_address"
